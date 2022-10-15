b041f36ab9dc57da21a36550584041d3b7b2f3ef
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-10-15 16:58:23 +0200

    Added third homework

---
+++ 3/Wireshark/Wireshark/Form1.cs
@@ -0,0 +1,76 @@
+using Microsoft.VisualBasic.FileIO;
+
+namespace Wireshark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        double numElement = -1;        //Header doesn't count
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            using (TextFieldParser parser = new TextFieldParser(@"AllAddressStatistics.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                //We want to calculate the distribution (univariate) on calories (int) 
+                Dictionary<string, int> rateDistribution = new Dictionary<string, int>();
+
+
+
+                while (!parser.EndOfData)
+                {
+                    //Process row
+                    numElement += 1;
+                    string[] fields = parser.ReadFields();
+                    int i = 0;      //Counter to know in wich field I am
+
+
+                    foreach (string field in fields)
+                    {
+
+                        richTextBox1.AppendText(field + " "); //Stampo a video il csv
+                        if (i == 8)  //I'm on the rate's field
+                        {
+
+                            if (rateDistribution.ContainsKey(field))
+                            {
+                                rateDistribution[field] += 1;
+                            }
+                            else
+                            {
+                                rateDistribution.Add(field, 1);
+                            }
+                        }
+
+                        i += 1;
+
+                    }
+
+
+                }
+
+                richTextBox1.AppendText("\n\nThe univariate distribution of field 'Rate (ms)' is :\n");
+
+
+
+                foreach (var item in rateDistribution)
+                {
+                    richTextBox1.AppendText("Attribute Value: " + item.Key + " Relative frequency: " + (item.Value / numElement) + "\n\n");
+                }
+            }
+        }
+    }
+}
